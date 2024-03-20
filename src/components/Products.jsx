b0eba71bc1d6ce9dd07d8bcf15e83f17c5e8e68c
Mark Saville <mark.saville@outlook.com>
2024-03-20 13:55:17 +0100

    Add Products component

---
+++ src/components/Products.jsx
@@ -0,0 +1,19 @@
+import "./Products.css";
+
+export default function Products() {
+  return (
+    <div>
+      <h2 className="page-title">Products</h2>
+      <div className="project-list">
+        <div className="checkbox-item">
+          <label>Electric Motors</label>
+          <input type="checkbox" />
+        </div>
+        <div className="checkbox-item">
+          <label>Communication Equipment</label>
+          <input type="checkbox" />
+        </div>
+      </div>
+    </div>
+  );
+}
