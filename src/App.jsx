9f5e7522ee6347ec474491bb322341d63366da18
Mark Saville <mark.saville@outlook.com>
2024-03-21 13:07:41 +0100

    Add modal functionality to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import Products from "./components/Products";
 import Navbar from "./components/Navbar";
+import Modalbox from "./components/Modalbox";
 
 import "./App.css";
 
 function App() {
+  // State to manage modal visibility
+  const [modalVisible, setModalVisible] = useState(false);
+
+  // Function to toggle modal visibility
+  const toggleModal = () => {
+    setModalVisible(!modalVisible);
+  };
+
   return (
     <div className="App">
-      <Navbar />
+      <Navbar toggleModal={toggleModal} />
       <Products />
+      {/* Render Modal component if modalVisible is true */}
+      {modalVisible && <Modalbox closeModal={toggleModal} />}
     </div>
   );
 }
