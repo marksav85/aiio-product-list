db8e9f9784c251590896ef21e036625cb09c8d23
Mark Saville <mark.saville@outlook.com>
2024-03-21 14:53:04 +0100

    Add click outside functionality to Modalbox component

--- src/components/Modalbox.jsx
+++ src/components/Modalbox.jsx
@@ -1,6 +1,23 @@
+import { useEffect } from "react";
 import "./Modalbox.css";
 
 export default function Modalbox({ closeModal }) {
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      const modal = document.querySelector(".modal");
+      if (modal && !modal.contains(event.target)) {
+        closeModal();
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+    console.log("Event listener added");
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [closeModal]);
+
   return (
     <div className="modal">
       <div className="modal-content">
