ec6e4b58cc74a93821c34ac5514b5cb5e7546421
Mark Saville <mark.saville@outlook.com>
2024-03-25 11:42:02 +0100

    Refactor button functioanlity for ModalNewProduct component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,32 @@
 import "./Navbar.css";
 
-export default function Navbar({ toggleModal }) {
-  const handleClick = (event) => {
+export default function Navbar({ toggleOrderList, toggleNewProduct }) {
+  const handleOrder = (event) => {
     event.stopPropagation(); // Stop the event from propagating to the document
-    toggleModal();
+    toggleOrderList();
   };
+
+  const handleNewProduct = (event) => {
+    event.stopPropagation(); // Stop the event from propagating to the document
+    toggleNewProduct();
+  };
+
   return (
     <div className="navbar">
       <ul>
         <li className="logo">
           <span>Product List</span>
         </li>
+        <li>
+          {/* Button to toggle New Product visibility */}
+          <button className="btn" onClick={handleNewProduct}>
+            Create SubProduct
+          </button>
+        </li>
 
         <li>
-          {/* Button to toggle modal visibility */}
-          <button className="btn" onClick={handleClick}>
+          {/* Button to toggle Order List visibility */}
+          <button className="btn" onClick={handleOrder}>
             Done
           </button>
         </li>
