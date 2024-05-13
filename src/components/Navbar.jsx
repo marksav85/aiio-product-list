994ee2be6b7c978b7cacea57a6d25de0eae16421
Mark Saville <mark.saville@outlook.com>
2024-05-13 15:22:18 +0200

    Remove create product button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,13 @@
 import "./Navbar.css";
+import { useCheckedProducts } from "../context/CheckedProductsContext";
 
 // Component for displaying the navigation bar with buttons to toggle visibility of order list and new product form
-export default function Navbar({ toggleOrderList, toggleNewProduct }) {
+export default function Navbar() {
+  const { toggleOrderList } = useCheckedProducts();
   // Function to handle toggling visibility of order list
   const handleOrder = (event) => {
     event.stopPropagation(); // Stop the event from propagating to the document
     toggleOrderList();
-  };
-
-  // Function to handle toggling visibility of new product form
-  const handleNewProduct = (event) => {
-    event.stopPropagation(); // Stop the event from propagating to the document
-    toggleNewProduct();
   };
 
   return (
@@ -20,12 +16,6 @@ export default function Navbar({ toggleOrderList, toggleNewProduct }) {
         {/* Logo */}
         <li className="logo">
           <span>Product List</span>
-        </li>
-        {/* Button to toggle New Product visibility */}
-        <li>
-          <button className="btn" onClick={handleNewProduct}>
-            Create SubProduct
-          </button>
         </li>
         {/* Button to toggle Order List visibility */}
         <li>
